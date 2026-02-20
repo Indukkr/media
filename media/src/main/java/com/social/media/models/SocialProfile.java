4c6c47d5edc27e7b4c71869cb10bf674e1d46b33
indu <patel.indu02@gmail.com>
2026-02-20 22:30:38 +0530

    One to One Relationship

---
+++ media/src/main/java/com/social/media/models/SocialProfile.java
@@ -0,0 +1,16 @@
+package com.social.media.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class SocialProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @OneToOne
+    @JoinColumn(name="social_user")
+    SocialUser user;
+
+}
